05deb7420a3215bdf0fe03e34e33a4c441396089
asus <asus@ASUS-PC>
2021-03-13 00:05:46 +0700

    Tahap pertama UI C#

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,9 +29,9 @@ namespace OutlookDemo
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
             this.guna2Button7 = new Guna.UI2.WinForms.Guna2Button();
-            this.guna2Button6 = new Guna.UI2.WinForms.Guna2Button();
             this.guna2Button5 = new Guna.UI2.WinForms.Guna2Button();
             this.guna2Button4 = new Guna.UI2.WinForms.Guna2Button();
             this.guna2Button3 = new Guna.UI2.WinForms.Guna2Button();
@@ -44,16 +44,17 @@ namespace OutlookDemo
             this.guna2Panel2 = new Guna.UI2.WinForms.Guna2Panel();
             this.guna2Panel3 = new Guna.UI2.WinForms.Guna2Panel();
             this.guna2Panel4 = new Guna.UI2.WinForms.Guna2Panel();
+            this.imgSlide = new Guna.UI2.WinForms.Guna2PictureBox();
             this.guna2Panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).BeginInit();
             this.guna2Panel2.SuspendLayout();
             this.guna2Panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgSlide)).BeginInit();
             this.SuspendLayout();
             // 
             // guna2Panel1
             // 
             this.guna2Panel1.Controls.Add(this.guna2Button7);
-            this.guna2Panel1.Controls.Add(this.guna2Button6);
             this.guna2Panel1.Controls.Add(this.guna2Button5);
             this.guna2Panel1.Controls.Add(this.guna2Button4);
             this.guna2Panel1.Controls.Add(this.guna2Button3);
@@ -61,11 +62,12 @@ namespace OutlookDemo
             this.guna2Panel1.Controls.Add(this.guna2Button1);
             this.guna2Panel1.Controls.Add(this.guna2HtmlLabel1);
             this.guna2Panel1.Controls.Add(this.guna2PictureBox1);
+            this.guna2Panel1.Controls.Add(this.imgSlide);
             this.guna2Panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.guna2Panel1.Location = new System.Drawing.Point(0, 0);
             this.guna2Panel1.Name = "guna2Panel1";
             this.guna2Panel1.ShadowDecoration.Parent = this.guna2Panel1;
-            this.guna2Panel1.Size = new System.Drawing.Size(180, 600);
+            this.guna2Panel1.Size = new System.Drawing.Size(220, 600);
             this.guna2Panel1.TabIndex = 0;
             // 
             // guna2Button7
@@ -73,6 +75,7 @@ namespace OutlookDemo
             this.guna2Button7.Animated = true;
             this.guna2Button7.BackColor = System.Drawing.Color.Transparent;
             this.guna2Button7.BorderRadius = 22;
+            this.guna2Button7.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton;
             this.guna2Button7.CheckedState.FillColor = System.Drawing.Color.White;
             this.guna2Button7.CheckedState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(118)))), ((int)(((byte)(212)))));
             this.guna2Button7.CheckedState.Parent = this.guna2Button7;
@@ -84,41 +87,22 @@ namespace OutlookDemo
             this.guna2Button7.Image = global::OutlookDemo.Properties.Resources.trash_can_60px;
             this.guna2Button7.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
             this.guna2Button7.ImageOffset = new System.Drawing.Point(10, 0);
-            this.guna2Button7.Location = new System.Drawing.Point(7, 461);
+            this.guna2Button7.Location = new System.Drawing.Point(7, 450);
             this.guna2Button7.Name = "guna2Button7";
             this.guna2Button7.ShadowDecoration.Parent = this.guna2Button7;
             this.guna2Button7.Size = new System.Drawing.Size(163, 43);
             this.guna2Button7.TabIndex = 1;
             this.guna2Button7.Text = "Trash";
             this.guna2Button7.UseTransparentBackground = true;
-            // 
-            // guna2Button6
-            // 
-            this.guna2Button6.BackColor = System.Drawing.Color.Transparent;
-            this.guna2Button6.BorderRadius = 22;
-            this.guna2Button6.CheckedState.FillColor = System.Drawing.Color.White;
-            this.guna2Button6.CheckedState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(118)))), ((int)(((byte)(212)))));
-            this.guna2Button6.CheckedState.Parent = this.guna2Button6;
-            this.guna2Button6.CustomImages.Parent = this.guna2Button6;
-            this.guna2Button6.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(118)))), ((int)(((byte)(212)))));
-            this.guna2Button6.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.guna2Button6.ForeColor = System.Drawing.Color.White;
-            this.guna2Button6.HoverState.Parent = this.guna2Button6;
-            this.guna2Button6.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
-            this.guna2Button6.ImageOffset = new System.Drawing.Point(10, 0);
-            this.guna2Button6.Location = new System.Drawing.Point(12, 461);
-            this.guna2Button6.Name = "guna2Button6";
-            this.guna2Button6.ShadowDecoration.Parent = this.guna2Button6;
-            this.guna2Button6.Size = new System.Drawing.Size(163, 43);
-            this.guna2Button6.TabIndex = 1;
-            this.guna2Button6.Text = "Inbox";
-            this.guna2Button6.UseTransparentBackground = true;
+            this.guna2Button7.CheckedChanged += new System.EventHandler(this.guna2Button1_Click);
+            this.guna2Button7.Click += new System.EventHandler(this.guna2Button7_Click);
             // 
             // guna2Button5
             // 
             this.guna2Button5.Animated = true;
             this.guna2Button5.BackColor = System.Drawing.Color.Transparent;
             this.guna2Button5.BorderRadius = 22;
+            this.guna2Button5.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton;
             this.guna2Button5.CheckedState.FillColor = System.Drawing.Color.White;
             this.guna2Button5.CheckedState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(118)))), ((int)(((byte)(212)))));
             this.guna2Button5.CheckedState.Parent = this.guna2Button5;
@@ -130,19 +114,22 @@ namespace OutlookDemo
             this.guna2Button5.Image = global::OutlookDemo.Properties.Resources.postal_100px;
             this.guna2Button5.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
             this.guna2Button5.ImageOffset = new System.Drawing.Point(10, 0);
-            this.guna2Button5.Location = new System.Drawing.Point(7, 412);
+            this.guna2Button5.Location = new System.Drawing.Point(7, 400);
             this.guna2Button5.Name = "guna2Button5";
             this.guna2Button5.ShadowDecoration.Parent = this.guna2Button5;
             this.guna2Button5.Size = new System.Drawing.Size(163, 43);
             this.guna2Button5.TabIndex = 1;
             this.guna2Button5.Text = "Draft";
             this.guna2Button5.UseTransparentBackground = true;
+            this.guna2Button5.CheckedChanged += new System.EventHandler(this.guna2Button1_Click);
+            this.guna2Button5.Click += new System.EventHandler(this.guna2Button5_Click);
             // 
             // guna2Button4
             // 
             this.guna2Button4.Animated = true;
             this.guna2Button4.BackColor = System.Drawing.Color.Transparent;
             this.guna2Button4.BorderRadius = 22;
+            this.guna2Button4.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton;
             this.guna2Button4.CheckedState.FillColor = System.Drawing.Color.White;
             this.guna2Button4.CheckedState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(118)))), ((int)(((byte)(212)))));
             this.guna2Button4.CheckedState.Parent = this.guna2Button4;
@@ -154,19 +141,22 @@ namespace OutlookDemo
             this.guna2Button4.Image = global::OutlookDemo.Properties.Resources.spam_60px;
             this.guna2Button4.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
             this.guna2Button4.ImageOffset = new System.Drawing.Point(10, 0);
-            this.guna2Button4.Location = new System.Drawing.Point(7, 363);
+            this.guna2Button4.Location = new System.Drawing.Point(7, 350);
             this.guna2Button4.Name = "guna2Button4";
             this.guna2Button4.ShadowDecoration.Parent = this.guna2Button4;
             this.guna2Button4.Size = new System.Drawing.Size(163, 43);
             this.guna2Button4.TabIndex = 1;
             this.guna2Button4.Text = "Spam";
             this.guna2Button4.UseTransparentBackground = true;
+            this.guna2Button4.CheckedChanged += new System.EventHandler(this.guna2Button1_Click);
+            this.guna2Button4.Click += new System.EventHandler(this.guna2Button4_Click);
             // 
             // guna2Button3
             // 
             this.guna2Button3.Animated = true;
             this.guna2Button3.BackColor = System.Drawing.Color.Transparent;
             this.guna2Button3.BorderRadius = 22;
+            this.guna2Button3.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton;
             this.guna2Button3.CheckedState.FillColor = System.Drawing.Color.White;
             this.guna2Button3.CheckedState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(118)))), ((int)(((byte)(212)))));
             this.guna2Button3.CheckedState.Parent = this.guna2Button3;
@@ -178,19 +168,22 @@ namespace OutlookDemo
             this.guna2Button3.Image = global::OutlookDemo.Properties.Resources.outbox_64px;
             this.guna2Button3.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
             this.guna2Button3.ImageOffset = new System.Drawing.Point(10, 0);
-            this.guna2Button3.Location = new System.Drawing.Point(7, 314);
+            this.guna2Button3.Location = new System.Drawing.Point(7, 300);
             this.guna2Button3.Name = "guna2Button3";
             this.guna2Button3.ShadowDecoration.Parent = this.guna2Button3;
             this.guna2Button3.Size = new System.Drawing.Size(163, 43);
             this.guna2Button3.TabIndex = 1;
             this.guna2Button3.Text = "Outbox";
             this.guna2Button3.UseTransparentBackground = true;
+            this.guna2Button3.CheckedChanged += new System.EventHandler(this.guna2Button1_Click);
+            this.guna2Button3.Click += new System.EventHandler(this.guna2Button3_Click);
             // 
             // guna2Button2
             // 
             this.guna2Button2.Animated = true;
             this.guna2Button2.BackColor = System.Drawing.Color.Transparent;
             this.guna2Button2.BorderRadius = 22;
+            this.guna2Button2.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton;
             this.guna2Button2.CheckedState.FillColor = System.Drawing.Color.White;
             this.guna2Button2.CheckedState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(118)))), ((int)(((byte)(212)))));
             this.guna2Button2.CheckedState.Parent = this.guna2Button2;
@@ -202,18 +195,21 @@ namespace OutlookDemo
             this.guna2Button2.Image = global::OutlookDemo.Properties.Resources.sent_100px;
             this.guna2Button2.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
             this.guna2Button2.ImageOffset = new System.Drawing.Point(10, 0);
-            this.guna2Button2.Location = new System.Drawing.Point(7, 265);
+            this.guna2Button2.Location = new System.Drawing.Point(7, 250);
             this.guna2Button2.Name = "guna2Button2";
             this.guna2Button2.ShadowDecoration.Parent = this.guna2Button2;
             this.guna2Button2.Size = new System.Drawing.Size(163, 43);
             this.guna2Button2.TabIndex = 1;
             this.guna2Button2.Text = "Sent";
             this.guna2Button2.UseTransparentBackground = true;
+            this.guna2Button2.CheckedChanged += new System.EventHandler(this.guna2Button1_Click);
+            this.guna2Button2.Click += new System.EventHandler(this.guna2Button2_Click);
             // 
             // guna2Button1
             // 
             this.guna2Button1.BackColor = System.Drawing.Color.Transparent;
             this.guna2Button1.BorderRadius = 22;
+            this.guna2Button1.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton;
             this.guna2Button1.Checked = true;
             this.guna2Button1.CheckedState.FillColor = System.Drawing.Color.White;
             this.guna2Button1.CheckedState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(118)))), ((int)(((byte)(212)))));
@@ -226,13 +222,15 @@ namespace OutlookDemo
             this.guna2Button1.Image = global::OutlookDemo.Properties.Resources.secured_letter_50px;
             this.guna2Button1.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
             this.guna2Button1.ImageOffset = new System.Drawing.Point(10, 0);
-            this.guna2Button1.Location = new System.Drawing.Point(7, 201);
+            this.guna2Button1.Location = new System.Drawing.Point(7, 200);
             this.guna2Button1.Name = "guna2Button1";
             this.guna2Button1.ShadowDecoration.Parent = this.guna2Button1;
             this.guna2Button1.Size = new System.Drawing.Size(163, 43);
             this.guna2Button1.TabIndex = 1;
             this.guna2Button1.Text = "Inbox";
             this.guna2Button1.UseTransparentBackground = true;
+            this.guna2Button1.CheckedChanged += new System.EventHandler(this.guna2Button1_Click);
+            this.guna2Button1.Click += new System.EventHandler(this.guna2Button1_Click);
             // 
             // guna2HtmlLabel1
             // 
@@ -274,7 +272,7 @@ namespace OutlookDemo
             this.guna2Button8.Image = global::OutlookDemo.Properties.Resources.logout_rounded_up_64px;
             this.guna2Button8.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
             this.guna2Button8.IndicateFocus = true;
-            this.guna2Button8.Location = new System.Drawing.Point(717, 12);
+            this.guna2Button8.Location = new System.Drawing.Point(677, 12);
             this.guna2Button8.Name = "guna2Button8";
             this.guna2Button8.PressedDepth = 0;
             this.guna2Button8.ShadowDecoration.Parent = this.guna2Button8;
@@ -289,7 +287,7 @@ namespace OutlookDemo
             this.guna2HtmlLabel2.BackColor = System.Drawing.Color.Transparent;
             this.guna2HtmlLabel2.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guna2HtmlLabel2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.guna2HtmlLabel2.Location = new System.Drawing.Point(640, 17);
+            this.guna2HtmlLabel2.Location = new System.Drawing.Point(600, 17);
             this.guna2HtmlLabel2.Margin = new System.Windows.Forms.Padding(0);
             this.guna2HtmlLabel2.Name = "guna2HtmlLabel2";
             this.guna2HtmlLabel2.Size = new System.Drawing.Size(267, 23);
@@ -301,30 +299,42 @@ namespace OutlookDemo
             // 
             this.guna2Panel2.Controls.Add(this.guna2Button8);
             this.guna2Panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.guna2Panel2.Location = new System.Drawing.Point(180, 0);
+            this.guna2Panel2.Location = new System.Drawing.Point(220, 0);
             this.guna2Panel2.Name = "guna2Panel2";
             this.guna2Panel2.ShadowDecoration.Parent = this.guna2Panel2;
-            this.guna2Panel2.Size = new System.Drawing.Size(892, 67);
+            this.guna2Panel2.Size = new System.Drawing.Size(852, 67);
             this.guna2Panel2.TabIndex = 1;
             // 
             // guna2Panel3
             // 
             this.guna2Panel3.Controls.Add(this.guna2HtmlLabel2);
             this.guna2Panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.guna2Panel3.Location = new System.Drawing.Point(180, 551);
+            this.guna2Panel3.Location = new System.Drawing.Point(220, 551);
             this.guna2Panel3.Name = "guna2Panel3";
             this.guna2Panel3.ShadowDecoration.Parent = this.guna2Panel3;
-            this.guna2Panel3.Size = new System.Drawing.Size(892, 49);
+            this.guna2Panel3.Size = new System.Drawing.Size(852, 49);
             this.guna2Panel3.TabIndex = 2;
             // 
             // guna2Panel4
             // 
             this.guna2Panel4.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.guna2Panel4.Location = new System.Drawing.Point(180, 67);
+            this.guna2Panel4.Location = new System.Drawing.Point(220, 67);
             this.guna2Panel4.Name = "guna2Panel4";
             this.guna2Panel4.ShadowDecoration.Parent = this.guna2Panel4;
-            this.guna2Panel4.Size = new System.Drawing.Size(892, 484);
+            this.guna2Panel4.Size = new System.Drawing.Size(852, 484);
             this.guna2Panel4.TabIndex = 3;
+            // 
+            // imgSlide
+            // 
+            this.imgSlide.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(118)))), ((int)(((byte)(212)))));
+            this.imgSlide.Image = ((System.Drawing.Image)(resources.GetObject("imgSlide.Image")));
+            this.imgSlide.Location = new System.Drawing.Point(158, 188);
+            this.imgSlide.Name = "imgSlide";
+            this.imgSlide.ShadowDecoration.Parent = this.imgSlide;
+            this.imgSlide.Size = new System.Drawing.Size(73, 68);
+            this.imgSlide.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imgSlide.TabIndex = 0;
+            this.imgSlide.TabStop = false;
             // 
             // Form1
             // 
@@ -348,6 +358,7 @@ namespace OutlookDemo
             this.guna2Panel2.ResumeLayout(false);
             this.guna2Panel3.ResumeLayout(false);
             this.guna2Panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgSlide)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -359,7 +370,6 @@ namespace OutlookDemo
         private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
         private Guna.UI2.WinForms.Guna2Button guna2Button1;
         private Guna.UI2.WinForms.Guna2Button guna2Button7;
-        private Guna.UI2.WinForms.Guna2Button guna2Button6;
         private Guna.UI2.WinForms.Guna2Button guna2Button5;
         private Guna.UI2.WinForms.Guna2Button guna2Button4;
         private Guna.UI2.WinForms.Guna2Button guna2Button3;
@@ -369,6 +379,7 @@ namespace OutlookDemo
         private Guna.UI2.WinForms.Guna2Panel guna2Panel2;
         private Guna.UI2.WinForms.Guna2Panel guna2Panel3;
         private Guna.UI2.WinForms.Guna2Panel guna2Panel4;
+        private Guna.UI2.WinForms.Guna2PictureBox imgSlide;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Guna.UI2.WinForms;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -18,13 +19,47 @@ namespace OutlookDemo
         }
 
 
+        private void moveImageBox(object sender)
+        {
+            Guna2Button b = (Guna2Button)sender;
+            imgSlide.Location = new Point(b.Location.X + 150, b.Location.Y - 10);
+            imgSlide.SendToBack();
 
+        }
 
         private void guna2Button8_Click(object sender, EventArgs e)
         {
             Close();
         }
 
-        
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            moveImageBox(sender);
+        }
+
+        private void guna2Button2_Click(object sender, EventArgs e)
+        {
+            moveImageBox(sender);
+        }
+
+        private void guna2Button3_Click(object sender, EventArgs e)
+        {
+            moveImageBox(sender);
+        }
+
+        private void guna2Button4_Click(object sender, EventArgs e)
+        {
+            moveImageBox(sender);
+        }
+
+        private void guna2Button5_Click(object sender, EventArgs e)
+        {
+            moveImageBox(sender);
+        }
+
+        private void guna2Button7_Click(object sender, EventArgs e)
+        {
+            moveImageBox(sender);
+        }
     }
 }
